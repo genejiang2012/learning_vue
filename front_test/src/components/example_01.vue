a763b2d93b8ccfc2fc2c06e02cf71687e554e18b
genejiang2012 <genejiang2012@outlook.com>
2021-04-30 16:22:39 +0800

    add the example

---
+++ front_test/src/components/example_01.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+        {{message}}
+    </div>
+</template>>
+
+<script>
+    var app = new vue(
+        {
+            el:'#app', 
+            data:{
+                message: 'Hello Vue!',
+            }
+        }
+    )
+</script>
--- front_test/src/router/index.js
+++ front_test/src/router/index.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: Gene Jiang
+ * @Date: 2021-04-29 14:44:20
+ * @LastEditors: Gene Jiang
+ * @LastEditTime: 2021-04-30 15:58:49
+ * @Description: 
+ */
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
@@ -10,6 +17,12 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    }, 
+    {
+      path: '/', 
+      name: 'example01', 
+      component: example_01
     }
+
   ]
 })
